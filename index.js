ae2ccc79f2ead3b3532c334c3e1a56139e534ed9
jurelik <jure@likar.me>
2019-01-25 22:43:00 +0000

    download mp3 from youtube and create an audio element

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-const fs = require('fs');
 const send = require('send');
 const express = require('express');
+const ytmp3 = require('youtube-mp3-downloader');
+
+const YD = new ytmp3({
+  'ffmpegpath': 'usr⁩/local⁩/Cellar⁩/ffmpeg',
+  'outputPath': './public/download/',
+  'youtubeVideoQuality': 'highest'
+});
 const app = express();
 
 app.use(express.static('public'));
 
-app.get('/stream', (req, res) => {
-  send(req, './public/comp 250.mp3').pipe(res);
+app.get('/stream/:id', (req, res) => {
+  send(req, `./public/download/${req.params.id}.mp3`).pipe(res);
 });
 
-app.get('/test', (req, res) => {
-  res.send('sup');
+app.get('/yt/:id', (req, res) => {
+  console.log('request made');
+  YD.download(req.params.id, `${req.params.id}.mp3`);
+  YD.on('finished', (err, data) => {
+    res.end('done');
+  });
 });
 
 app.listen(3000, () => {
--- public/main.js
+++ public/main.js
@@ -1,11 +1,22 @@
 const audioTag = document.getElementById('audio-tag');
-const audioElem = document.createElement('audio');
 
-function create() {
+function create(id) {
+  const audioElem = document.createElement('audio');
   audioElem.setAttribute('controls', '');
+  audioElem.setAttribute('src', `http://localhost:3000/stream/${id}`);
   document.body.appendChild(audioElem);
 };
 
 function update() {
   audioElem.setAttribute('src', 'http://localhost:3000/stream');
+}
+
+function get(id) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `http://localhost:3000/yt/${id}`, true);
+  request.onload = function() {
+    console.log(request.response);
+    create(id);
+  };
+  request.send();
 }
